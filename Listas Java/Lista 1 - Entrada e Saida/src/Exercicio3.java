d1cc90baf2d37e1ae3091ca027c24da34bd56e47
LeticiaRoth <leticiaroth1@gmail.com>
2025-09-12 16:34:59 -0300

    Exercicios Java

--- Listas Java/Lista 1 - Entrada e Saida/src/Exercicio3.java
+++ Listas Java/Lista 1 - Entrada e Saida/src/Exercicio3.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Exercicio3 {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("Digite o valor da sua compra:");
+        double valor_compra = read.nextDouble();
+
+        double representantes = (20.0 / 100) * valor_compra;
+        double impostos = (30.0 / 100) * valor_compra;
+        double valor_venda = valor_compra + representantes + impostos;
+
+        //Atribuições
+        System.out.println("Valor de compra: R$ " + valor_compra);
+        System.out.println("Valor do representante (20%): R$ " + representantes);
+        System.out.println("Valor dos impostos (30%): R$ " + impostos);
+        System.out.println("Valor de venda: R$ " + valor_venda);
+
 
     }
 }
