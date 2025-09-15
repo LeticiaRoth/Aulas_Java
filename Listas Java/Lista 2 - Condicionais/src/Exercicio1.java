60457f86cafa8dd57c450db14acffd24c17adacd
LeticiaRoth <leticiaroth1@gmail.com>
2025-09-15 15:47:50 -0300

    Exercicios da lista 2 - condicionais

---
+++ Listas Java/Lista 2 - Condicionais/src/Exercicio1.java
@@ -0,0 +1,11 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.in.print("Digite sua idade: ");
+        int idade = read.nextInt(System.in);
+
+        System.out.printf("%d", idade);
+
+    }
+}
